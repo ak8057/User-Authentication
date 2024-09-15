2b87032a484c7081888a80730a01060479694c7a
Abhay Kumar <abhay292005@gmail.com>
2024-09-15 09:21:20 +0530

    Initial commit: server setup database conn error handling

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,44 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import createError from "../utils/appError.js";
+
+//!REGISTER USER
+const signup = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return next(new createError("User already exists!", 400));
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await User.create({
+        ...req.body,
+        password: hashedPassword,
+    });
+
+    //!assign jwt token to user(json web token)
+    const token = jwt.sign({ _id: newUser._id }, "secretkey123", {
+        expiresIn: "90d",
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: "User registered succesfully",
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+const login = async (req, res, next) => {
+  try{
+
+  }catch
+  
+};
+
+export default {signup, login}
