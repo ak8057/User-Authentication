4c21cc0b8e7b6b5c4222899572a56b66757270fe
Abhay Kumar <abhay292005@gmail.com>
2024-10-12 13:32:20 +0530

    all updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import { useAuth } from "./contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+const App = () => {
+  const { isAuthenticated } = useAuth();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !isAuthenticated ? <Register /> : <Navigate to="/dashboard" />
+          }
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Login />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/Auth/Register.jsx
@@ -0,0 +1,134 @@
+// import React from "react";
+// import { Card, Flex, Input, Typography } from "antd";
+
+// const Register = () => {
+//   const handleRegister = (values) => {
+//     console.log(values);
+//   };
+//   return (
+//     <Card className="form-container">
+//       <Flex>
+//         <Flex>
+//             <Flex vertical flex={1}>
+//           <Typography.Title leverl={3} strong className="title">
+//             Create an Account
+//           </Typography.Title>
+//           <Typography.Text type="secondary" strong className="slogan">
+//             Join for exclusive access
+//           </Typography.Text>
+
+//           <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
+//             <Form.Item
+//               Label="Full Name"
+//               name="name"
+//               rules={[
+//                 { required: true, message: "Please input your full name" },
+//               ]}>
+//               <Input placeholder="Full Name" />
+//             </Form.Item>
+//           </Form>
+//           </Flex>
+//         </Flex>
+//       </Flex>
+//     </Card>
+//   );
+// };
+
+// export default Register;
+
+
+
+import React from "react";
+import {Alert, Spin,  Card, Col, Form, Input, Row, Typography, Button } from "antd";
+import { Link } from "react-router-dom";
+import registerimg from "../assets/istockphoto-1286100689-612x612.jpg";
+import useSignup from "../hooks/useSignup.jsx";
+
+
+
+const Register = () => {
+  const {loading,error, registerUser} =useSignup();
+  const handleRegister = (values) => {
+    registerUser(values);
+    // console.log(values);
+  };
+
+  return (
+    <Card className="form-container">
+      <Row className="register-row" gutter={[40, 0]}>
+        <Col xs={24} lg={12} className="form-column">
+          <Typography.Title level={3} className="title">
+            Create an Account
+          </Typography.Title>
+          <Typography.Text type="secondary" className="slogan">
+            Join for exclusive access
+          </Typography.Text>
+
+          <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
+            <Form.Item
+              label="Full Name"
+              name="name"
+              rules={[{ required: true, message: "Please input your full name" }]}
+            >
+              <Input size="large" placeholder="Enter your Full Name" />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, type: "email", message: "Please enter a valid email" }]}
+            >
+              <Input size="large" placeholder="Enter your Email" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: "Please input your password" }]}
+            >
+              <Input.Password size="large" placeholder="Password" />
+            </Form.Item>
+            <Form.Item
+              label="Confirm Password"
+              name="passwordConfirm"
+              rules={[{ required: true, message: "Please input your password" }]}
+            >
+              <Input.Password size="large" placeholder="Confirm Password" />
+            </Form.Item>
+
+            {error && (
+                <Alert description={error} type="error" showIcon closable className="alert"/>
+            )}
+        
+
+            <Form.Item>
+              <Button 
+              type={`${loading ? '' : 'primary'}`} 
+              htmlType="submit" size="large" className="btn">
+                {loading ? <Spin/> : 'Create Account'}
+               
+              </Button>
+            </Form.Item>
+
+            <Form.Item className="signin-link">
+              <Typography.Text>
+                Already have an account?&nbsp;
+                <Link to="/login">
+                  <Button type="link" className="btn-link">Sign In</Button>
+                </Link>
+              </Typography.Text>
+            </Form.Item>
+          </Form>
+        </Col>
+
+        <Col xs={0} lg={12} className="image-column">
+          <img src={registerimg} className="auth-img" alt="Register" />
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default Register;
+
+
